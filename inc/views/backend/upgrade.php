4964f3c5eb0c112730d2956252e0dcb06446893c
WordPress.org SVN <noreply@wordpress.org>
2022-07-04 10:02:47 +0000

    Import WordPress.org tag 1.1.9

--- inc/views/backend/upgrade.php
+++ inc/views/backend/upgrade.php
@@ -1,4 +1,4 @@
 <div class="stdl-go-pro">
-    <a href="https://1.envato.market/Q3KGo" target="_blank"><img src="<?php echo STDL_URL . 'images/upgradeImg.png' ?>" /></a>
+    <a href="https://1.envato.market/Q3KGo" target="_blank"><img src="<?php echo STDL_URL . 'images/upgrade-to-pro-banner.png' ?>" /></a>
     <a href="https://1.envato.market/Q3KGo" class="stdl-button-orange" target="_blank"><?php esc_html_e('Upgrade now', 'subscribe-to-download-lite'); ?></a>
 </div>
